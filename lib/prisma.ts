c62369ac9435f10acbab1ffb213c2cf5f4356768
imon-n <imon.eeecu@gmail.com>
2026-05-14 17:57:14 +0600

    fix prisma setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,34 @@
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// // import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({
+  connectionString,
+});
 
-export { prisma };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}

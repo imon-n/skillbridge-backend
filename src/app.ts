bf7951cc2f4dce4cfa1dc5a9a69a2ac800792df6
imon-n <imon.eeecu@gmail.com>
2026-05-27 21:01:54 +0600

    added heree

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(
 );
 
 
-app.use("/api/auth", toNodeHandler(auth)); 
+app.all("/api/auth/*", toNodeHandler(auth)); // 👈 এটা
 app.use("/api", authRoute);                 
 
 // other routes
